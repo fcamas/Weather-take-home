07e1aad6b130fb21bac67c54860ed1a444386e3f
Fredy <fredycamasdev@gmail.com>
2024-11-22 20:58:10 -0500

    add initial files

---
+++ Weather-take-home/Weather_take_homeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_take_homeApp.swift
+//  Weather-take-home
+//
+//  Created by Fredy on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_take_homeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Weather-take-homeTests/Weather_take_homeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_take_homeTests.swift
+//  Weather-take-homeTests
+//
+//  Created by Fredy on 11/22/24.
+//
+
+import Testing
+@testable import Weather_take_home
+
+struct Weather_take_homeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
